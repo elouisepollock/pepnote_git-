e75f6578ce6f71380905ba41ff09691de6558587
elouise <elouise.l.pollock@gmail.com>
2018-05-09 15:50:13 +1000

    copied jens image migrate file

--- db/migrate/20180508022029_add_image_to_items.rb
+++ db/migrate/20180508022029_add_image_to_items.rb
@@ -1,5 +1,5 @@
 class AddImageToItems < ActiveRecord::Migration[5.1]
     def change
-        add_reference :items, :image, foreign_key: true
+      add_column :items, :image, :string
     end
-end
+  end
