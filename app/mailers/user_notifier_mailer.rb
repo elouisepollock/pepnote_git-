4944ad058fdb0dfbafd1d4a4b3c00dbb43433a8a
elouise <elouise.l.pollock@gmail.com>
2018-05-09 19:15:37 +1000

    fixed sendgrid

--- app/mailers/user_notifier_mailer.rb
+++ app/mailers/user_notifier_mailer.rb
@@ -1,12 +1,20 @@
 class UserNotifierMailer < ApplicationMailer
-    class UserNotifier < ActionMailer::Base
-        default :from => 'any_from_address@example.com'
+#     # class UserNotifier < ActionMailer::Base
+#         default :from => 'any_from_address@example.com'
       
-        # send a signup email to the user, pass in the user object that   contains the user's email address
-        def send_signup_email(user)
-          @user = user
-          mail( :to => @user.email,
-          :subject => 'Thanks for signing up for our amazing app' )
-        end
-      end
+#         # send a signup email to the user, pass in the user object that   contains the user's email address
+#         def send_signup_email(user)
+#           @user = user
+#           mail( :to => @user.email,
+#           :subject => 'Thanks for signing up for our amazing app' )
+#         end
+#       end
+# end
+
+  default :from => 'any_from_address@example.com'
+  def send_signup_email(recipient)
+    mail( :to =>recipient.email,
+    :subject =>'New Listing Added' )
+
+  end
 end
